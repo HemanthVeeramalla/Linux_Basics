a97fff3bd072d73d186fbb51860067c6a2fa92eb
hemanthveeramalla <hemanthveeramalla@mirafra.com>
2022-08-03 04:10:45 +0530

    first commit

---
+++ demo0.v
@@ -0,0 +1,12 @@
+#code for xnor logic gate
+
+module xnor_gate(out,a,b);
+
+#input and output port declarations
+input a,b;	#inputs for loic gate
+output out;	#output for logic gate
+
+#dataflow modelling style code
+assign out=~(a^b);
+
+endmodule
